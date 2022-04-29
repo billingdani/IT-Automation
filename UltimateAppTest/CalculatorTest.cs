41f225a2dc74546f65481c5c57e970c2d0659f76
IT Üzemeltetés <itlab@aut.bme.hu>
2022-04-29 11:31:34 +0200

    All tests done, modulo error corrected

--- UltimateAppTest/CalculatorTest.cs
+++ UltimateAppTest/CalculatorTest.cs
@@ -16,5 +16,29 @@ namespace UltimateApp.Test
             int? retVal = Calculator.calculate(2, 3, Operation.Add);
             Assert.AreEqual(5, retVal);
         }
+        [TestMethod]
+        public void SubtractTest()
+        {
+            int? retVal = Calculator.calculate(3, 2, Operation.Subtract);
+            Assert.AreEqual(1, retVal);
+        }
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Multiply);
+            Assert.AreEqual(6, retVal);
+        }
+        [TestMethod]
+        public void DivideTest()
+        {
+            int? retVal = Calculator.calculate(6, 2, Operation.Divide);
+            Assert.AreEqual(3, retVal);
+        }
+        [TestMethod]
+        public void ModuloTest()
+        {
+            int? retVal = Calculator.calculate(3, 2, Operation.Modulo);
+            Assert.AreEqual(1, retVal);
+        }
     }
 }
